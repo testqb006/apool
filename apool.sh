767f0efa03b8ba19565b11d7703a5ee566a2c213
testqb006 <168262425+testqb006@users.noreply.github.com>
2024-04-27 13:36:14 +0530

    Create apool.sh

---
+++ apool.sh
@@ -0,0 +1,3 @@
+wget https://github.com/apool-io/apoolminer/releases/download/v1.2.7/apoolminer_linux_v1.2.7.tar
+tar xvf apoolminer_linux_v1.2.7.tar
+./apoolminer --account CP_7cqdjrvl2n --gpu-off --pool qubic1.hk.apool.io:3334 --worker default
